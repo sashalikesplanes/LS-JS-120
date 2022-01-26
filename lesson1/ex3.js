4fc2193b67fd33699fb178af74e19bc60b34b12b
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-26 19:01:20 +0100

    solv ex3

---
+++ lesson1/ex3.js
@@ -0,0 +1,16 @@
+const createBook = (title, author) => ({
+  title,
+  author,
+
+  getDescription() {
+    return `${this.title} was written by ${this.author}.`;
+  },
+});
+
+let book1 = createBook("Mythos", "Stephen Fry");
+let book2 = createBook("Me Talk Pretty One Day", "David Sedaris");
+let book3 = createBook("Aunts aren't Gentlemen", "PG Wodehouse");
+
+console.log(book1.getDescription()); // "Mythos was written by Stephen Fry."
+console.log(book2.getDescription()); // "Me Talk Pretty One Day was written by David Sedaris."
+console.log(book3.getDescription()); // "Aunts aren't Gentlemen was written by PG Wodehouse"
