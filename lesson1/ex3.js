77756f1149b40191cc74e7572dc6fb48a15bbdbd
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-26 19:05:24 +0100

    solved 7

--- lesson1/ex3.js
+++ lesson1/ex3.js
@@ -4,7 +4,10 @@ const createBook = (title, author, read = false) => ({
   read,
 
   getDescription() {
-    return `${this.title} was written by ${this.author}.`;
+    let description = `${this.title} was written by ${this.author}. `;
+    if (this.read) description += "I have read it.";
+    else description += "I haven't read it.";
+    return description;
   },
 
   readBook() {
@@ -16,14 +19,6 @@ let book1 = createBook("Mythos", "Stephen Fry");
 let book2 = createBook("Me Talk Pretty One Day", "David Sedaris");
 let book3 = createBook("Aunts aren't Gentlemen", "PG Wodehouse");
 
-console.log(book1.getDescription()); // "Mythos was written by Stephen Fry."
-console.log(book2.getDescription()); // "Me Talk Pretty One Day was written by David Sedaris."
-console.log(book3.getDescription()); // "Aunts aren't Gentlemen was written by PG Wodehouse"
-
-console.log(book1.read); // => false
-console.log(book2.read); // => false
-console.log(book3.read); // => false
-
-console.log(book1.read); // => false
+console.log(book1.getDescription()); // Mythos was written by David Fry. I haven't read it.
 book1.readBook();
-console.log(book1.read); // => true
+console.log(book1.getDescription()); //
