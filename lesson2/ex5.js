8973a930d2a3ef3a4f0f26cb9233600b00e99537
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-28 13:46:21 +0100

    solved exercise

---
+++ lesson2/ex5.js
@@ -0,0 +1,20 @@
+let turk = {
+  firstName: "Christopher",
+  lastName: "Turk",
+  occupation: "Surgeon",
+  getDescription() {
+    return (
+      this.firstName + " " + this.lastName + " is a " + this.occupation + "."
+    );
+  },
+};
+
+function logReturnVal(func, context) {
+  let returnVal = func.call(context);
+  console.log(returnVal);
+}
+
+logReturnVal(turk.getDescription, turk);
+
+let getDescriptionTurk = turk.getDescription.bind(turk);
+console.log(getDescriptionTurk());
