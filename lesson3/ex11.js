dc4f391b23c649eb53b3702a7634854e6dd6ec06
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-30 18:07:55 +0100

    solved exercise

---
+++ lesson3/ex11.js
@@ -0,0 +1,18 @@
+function Ninja() {
+  this.swung = false;
+}
+
+// Add a swing method to the Ninja prototype which
+// modifies `swung` and returns the calling object
+
+Ninja.prototype.swing = function () {
+  this.swung = !this.swung;
+  return this; // Required to get a prop of the object
+  // on which the method is invoked
+};
+
+let ninjaA = new Ninja();
+let ninjaB = new Ninja();
+
+console.log(ninjaA.swing().swung); // logs `true`
+console.log(ninjaB.swing().swung); // logs `true`
