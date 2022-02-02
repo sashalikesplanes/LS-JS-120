328ab3d444233c386c387642565389a65789e9ed
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-02 21:28:26 +0100

    removed magic numbers and arrays and added customizable player name

--- lesson5/twentyOne.js
+++ lesson5/twentyOne.js
@@ -1,35 +1,3 @@
-// Description
-
-// The cards are all dealt from a classic 52 card deck
-// A player and a dealer are dealt 2 cards each
-// The player is shown one of dealers card
-// The player can choose to hit where they are given another card
-// Or can choose to stay
-// If the players total adds to more than 21 then they are bust and lose
-// Otherwise the dealer hits until they are at or above a total of 17
-// If the dealer gets more than 21 they are bust and the player wins
-// Otherwise the winner is determined by who has a higher total
-// Cards 2 - 10 are at face value
-// J, Q, K are worth 10 each
-// Ace is worth 11 or 1 depending on whether the player goes bust
-
-// N: Deck of cards, player, dealer, hand, card, worth
-// V: Shuffle deck, deal card, hit, stay, get total, compare totals
-
-// Deck - n
-//  dealCard - v
-//  shuffleDeck - v
-
-// Participant - n
-//  Dealer - n
-//  Player - n
-//  makeMove - v
-
-// Game - n
-//  dealCards - v
-//  compareTotals - v
-//  declareWinner - v
-
 const readline = require("readline-sync");
 
 class ClassicCard {
@@ -61,6 +29,8 @@ class ClassicCard {
     "A",
   ];
   static VALID_SUITS = ["S", "H", "C", "D"];
+  static ACE_CARD = "A";
+  static FACE_CARDS = ["J", "Q", "K"];
 
   constructor(value, suit) {
     this.value = value;
@@ -142,23 +112,37 @@ class CardHolder {
   }
 }
 
+class Player extends CardHolder {
+  constructor() {
+    super();
+    this.setPlayerName();
+  }
+
+  setPlayerName() {
+    console.log('Please enter your name and press "ENTER"');
+    let name = readline.prompt();
+    this.name = name;
+  }
+}
+
 class TwentyOneGame {
   static NUM_OF_CARDS_AT_START = 2;
   static DEALER_NAME = "Dealer";
-  static PLAYER_NAME = "Player";
   static DECK_SIZE = 52;
+  static FACE_CARD_VALUE = 10;
+  static ACE_CARD_VALUE = 11;
+  static BUST_VALUE = 21;
+  static DEALER_STAY_VALUE = 17;
 
   constructor() {
+    this.printWelcome();
     this.dealer = new CardHolder(TwentyOneGame.DEALER_NAME);
-    this.player = new CardHolder(TwentyOneGame.PLAYER_NAME);
+    this.player = new Player();
     this.deck = new Deck(TwentyOneGame.DECK_SIZE);
   }
 
   play() {
-    this.printWelcome();
-
     this.dealHands([this.dealer, this.player]);
-
     this.printHands([this.dealer, this.player]);
 
     while (this.playerHits()) {
@@ -180,18 +164,19 @@ class TwentyOneGame {
 
   printWelcome() {
     console.clear();
-    console.log("Welcome to the game of 21");
+    console.log(`Welcome to the game of ${TwentyOneGame.BUST_VALUE}`);
     console.log(
       "The goal is to get a higher hand value than the dealer, " +
-        "but not more than 21, otherwise you're bust!"
+        `but not more than ${TwentyOneGame.BUST_VALUE}, otherwise you're bust!`
     );
     console.log(
-      "2 - 10  are worth their face values, Jack, Queen and King are worth 10 each " +
-        "an Ace is worth either 11 or 1 depending on if you're over 21"
+      `2 - 10  are worth their face values
+      Jack, Queen and King are worth ${TwentyOneGame.FACE_CARD_VALUE} each
+      Ace is worth either ${TwentyOneGame.ACE_CARD_VALUE} or 1 depending on if you're over 21`
     );
     console.log(
-      "You can always choose to hit or stay, the dealer stays " +
-        "once their hand is worth 17 or more."
+      `You can always choose to hit or stay, the dealer stays ` +
+        `once their hand is worth ${TwentyOneGame.DEALER_STAY_VALUE} or more.`
     );
     console.log('Press "ENTER" to start the game.');
     readline.prompt();
@@ -252,24 +237,32 @@ class TwentyOneGame {
   }
 
   dealerHits() {
-    if (this.getHandValue(this.dealer.getHand()) < 17) return true;
-    else return false;
+    if (
+      this.getHandValue(this.dealer.getHand()) < TwentyOneGame.DEALER_STAY_VALUE
+    ) {
+      return true;
+    } else return false;
   }
 
   isBust(player) {
-    if (this.getHandValue(player.getHand()) > 21) return true;
-    else return false;
+    if (this.getHandValue(player.getHand()) > TwentyOneGame.BUST_VALUE) {
+      return true;
+    } else return false;
   }
 
   getCardValue(card) {
     if (!Number.isNaN(Number(card.value))) return Number(card.value);
-    else if (["J", "K", "Q"].includes(card.value)) return 10;
-    else return 11;
+    else if (ClassicCard.FACE_CARDS.includes(card.value)) {
+      return TwentyOneGame.FACE_CARD_VALUE;
+    } else return TwentyOneGame.ACE_CARD_VALUE;
   }
 
   getHandNumOfAces(hand) {
     let countOfAces = 0;
-    hand.forEach((card) => (countOfAces += card.getValue() === "A" ? 1 : 0));
+    hand.forEach(
+      (card) =>
+        (countOfAces += card.getValue() === ClassicCard.ACE_CARD ? 1 : 0)
+    );
     return countOfAces;
   }
 
@@ -277,8 +270,8 @@ class TwentyOneGame {
     let value = 0;
     let numOfAces = this.getHandNumOfAces(hand);
     hand.forEach((card) => (value += this.getCardValue(card)));
-    while (value > 21 && numOfAces > 0) {
-      value -= 10;
+    while (value > TwentyOneGame.BUST_VALUE && numOfAces > 0) {
+      value -= TwentyOneGame.ACE_CARD_VALUE - 1; // Make ace worth 1
       numOfAces -= 1;
     }
     return value;
