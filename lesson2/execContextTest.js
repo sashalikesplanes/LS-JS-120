69ddc9a7e4ae32914421189fb2b98bed7a4ce38e
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-03 20:38:28 +0100

    performed more tests

--- lesson2/execContextTest.js
+++ lesson2/execContextTest.js
@@ -15,3 +15,21 @@ class Test {
 }
 
 let test = new Test();
+
+class Bar {
+  constructor() {
+    this.bar = 1;
+  }
+
+  qux() {
+    console.log("hi");
+  }
+}
+class Foo extends Bar {
+  foo = function () {
+    console.log(this.__proto__);
+  };
+}
+
+let foo = new Foo();
+console.log("foo" in foo);
